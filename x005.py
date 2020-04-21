e11107454b0881afa4ef65b1f8304dbef41473a0
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-21 18:54:06 +0530

    new

--- x005.py
+++ x005.py
@@ -1,19 +1,19 @@
-#Smallest Multiple
-def multiple(x,y):
-    c = 2
-    lst = [True]
-    for i in range(x+1,y+1):
-        for j in lst:
-            if (j == True):
-                if c % i == 0:
-                    lst.append(True)
-                else: lst.append(False)
-            else:
-                c += 1
-    print(lst)
-    print(str(c))
+def findSmallestMultiple(n):
+    for i in range(n, factorial(n) + 1, n):
+        if isMultiple(i, n):
+            return i
+    return -1
 
+def isMultiple(x, n):
+    for i in range(1, n):
+        if x % i != 0:
+            return False
+    return True
 
+def factorial(n):
+    if n > 1: return n * factorial(n - 1)
+    elif n >= 0: return 1
+    else: return -1
 
-
-print(multiple(1,3))
+print (findSmallestMultiple(10)) # 2520
+print (findSmallestMultiple(20))
