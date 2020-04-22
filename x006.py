366bcf4ba853dad9ce3bed3a802de7fb7e539eaf
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-22 15:03:05 +0530

    difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.

--- x006.py
+++ x006.py
@@ -1,1 +1,10 @@
 #sum square
+def sum_square(n):
+    x = (n * (n+1) /2)**2
+    y =  n*(n+1)*((2*n)+1)/6
+    print("The sum of the squares of the first n natural numbers is",y)
+    print("The square of the sum of the first n natural numbers is",x)
+    return x - y
+
+
+print("The difference is ",sum_square(10)) # 385,3025,2640
