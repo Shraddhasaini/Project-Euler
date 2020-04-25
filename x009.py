7946c41bb86d12c7b986f4277893cca2337e60f8
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-25 14:19:14 +0530

    laf

--- x009.py
+++ x009.py
@@ -7,9 +7,9 @@ def Triplet(n):#O(N^2)
             al = lambda a,b,c : b if (max(a,b,c)==a) else (c if (max(a,b,c)==b) else a)
             base = lambda a,b,c : c if (max(a,b,c)==a) else (a if (max(a,b,c)==b) else b)
             if (hypo(a,b,c)**2 == al(a,b,c)**2 + base(a,b,c)**2):
-                print("hypotenuse is:", hypo(a,b,c))
+                print("Hypotenuse is:", hypo(a,b,c))
                 print("Altitude is:", al(a,b,c))
-                print("Base is", base(a,b,c))
+                print("Base is:", base(a,b,c))
                 return hypo(a,b,c)*al(a,b,c)*base(a,b,c)
 
 
