bc82d7996189b08579a8cf0fe4a2392998d929fc
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-25 14:17:18 +0530

    reducing loc

--- x009.py
+++ x009.py
@@ -1,15 +1,12 @@
 
-def Triplet(n):
+def Triplet(n):#O(N^2)
     for a in range(1, 400):
         for b in range(1, 400):
-            c = (1000 - a) - b
+            c = (n - a) - b
             hypo = lambda a,b,c : a if (max(a,b,c)==a) else (b if (max(a,b,c)==b) else c)
             al = lambda a,b,c : b if (max(a,b,c)==a) else (c if (max(a,b,c)==b) else a)
             base = lambda a,b,c : c if (max(a,b,c)==a) else (a if (max(a,b,c)==b) else b)
-            x = hypo(a,b,c)**2
-            y = al(a,b,c)**2
-            z = base(a,b,c)**2
-            if (x == y +z):
+            if (hypo(a,b,c)**2 == al(a,b,c)**2 + base(a,b,c)**2):
                 print("hypotenuse is:", hypo(a,b,c))
                 print("Altitude is:", al(a,b,c))
                 print("Base is", base(a,b,c))
