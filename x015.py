bdcf4d7aa7230f22c0ec5bf90cc3f3fa15064d65
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-30 12:50:36 +0530

    ..

--- x015.py
+++ x015.py
@@ -1,10 +1,7 @@
 n=[[1]*21]*21
-#print n[0][1]
-#print n[2][0]
 k=21
 for i in range(1,k):
     for j in range(1,k):
         n[i][j]=n[i-1][j]+n[i][j-1]
-        #print 'n[',i,'][',j,'] = ',n[i][j]
 
 print (n[20][20])
