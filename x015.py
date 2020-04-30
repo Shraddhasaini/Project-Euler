a7847e3bfc1928cd23d62837cbc61113e3473565
Shraddhasaini <shraddhasaini99@gmail.com>
2020-04-30 12:33:49 +0530

    route

--- x015.py
+++ x015.py
@@ -1,7 +1,10 @@
-import numpy as np
-grid = np.arange(9).reshape(3,3)
-print(grid)
-count = 0
+n=[[1]*21]*21
+#print n[0][1]
+#print n[2][0]
+k=21
+for i in range(1,k):
+    for j in range(1,k):
+        n[i][j]=n[i-1][j]+n[i][j-1]
+        #print 'n[',i,'][',j,'] = ',n[i][j]
 
-def route(0,8):
-    pass
+print (n[20][20])
