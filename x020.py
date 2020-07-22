70ae24fe5b68501310261b863de1417e1848464a
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-22 10:46:22 +0530

    problem 20

--- x020.py
+++ x020.py
@@ -2,7 +2,9 @@ def fac(x):
     mul = 1
     for i in range(2,x+1):
         mul = mul*i
-    print(mul)
-    
+    lst = []
+    for i in range(0,len(str(mul))):
+        lst.append(int(str(mul)[i]))
+    print(sum(lst))
 
-print(fac(10))
+print(fac(100))
