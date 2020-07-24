56a35df19e148f1d80b5192438e5716c299f501d
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-24 10:06:22 +0530

    %

--- x021.py
+++ x021.py
@@ -1,5 +1,17 @@
 #Amicable numbers
 #getting divisors
+amicable = []
+
 def divisors(x):
     lstd=[1]
-    
+    for i in range(2,x):
+        if x%i == 0:
+            lstd.append(i)
+    add = sum(lstd)
+
+
+def amicable():
+
+
+
+print(amicable())
