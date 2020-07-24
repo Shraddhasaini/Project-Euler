10ced5c8a511b625fade5d66bd5a375014e3b11a
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-24 10:13:36 +0530

    unoptimized

--- x021.py
+++ x021.py
@@ -1,17 +1,25 @@
 #Amicable numbers
 #getting divisors
-amicable = []
+Amicable = []
 
 def divisors(x):
     lstd=[1]
     for i in range(2,x):
         if x%i == 0:
             lstd.append(i)
-    add = sum(lstd)
+    return sum(lstd)
+
 
 
 def amicable():
-
+    for i in range(1,10000):
+        sumres = divisors(i)
+        for j in range(i,10000):
+            sumres1 = divisors(j)
+            if (sumres == j) and (sumres1 == i):
+                Amicable.append(sumres)
+                Amicable.append(sumres1)
+    return Amicable
 
 
 print(amicable())
