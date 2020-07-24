4c0ec60fa8bcbfd66869671a49e8740410d8f3a7
Shraddhasaini <shraddhasaini99@gmail.com>
2020-07-24 10:15:54 +0530

    amicable optimized

--- x021.py
+++ x021.py
@@ -1,25 +1,33 @@
 #Amicable numbers
-#getting divisors
-Amicable = []
+import math
+list=[0]*10001
+def nod(n):
+    count=0
+    sum=-n
+    m=math.sqrt(n)
+    i=1
+    while i<m:
+        if n%i==0:
+            sum+=i+(n/i)
+        i+=1
 
-def divisors(x):
-    lstd=[1]
-    for i in range(2,x):
-        if x%i == 0:
-            lstd.append(i)
-    return sum(lstd)
-
-
-
-def amicable():
-    for i in range(1,10000):
-        sumres = divisors(i)
-        for j in range(i,10000):
-            sumres1 = divisors(j)
-            if (sumres == j) and (sumres1 == i):
-                Amicable.append(sumres)
-                Amicable.append(sumres1)
-    return Amicable
+    if int(m)==m:
+        sum+=m
+    if n==1:sum=1
+    return int(sum)
 
 
-print(amicable())
+n=10000
+i=0
+sum=0
+while i<n:
+    if list[i]!=0:
+        x=nod(i)
+        if x==list[i]:
+            sum+=list[i]+i
+    else:
+        x=nod(i)
+        if x!=1 and x<10000:
+            list[x]=i
+    i+=1
+print sum
